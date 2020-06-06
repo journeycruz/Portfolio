a58c8eb3a3a982073d49a3c713f90b07f3f5591a
Journey Cruz <journey.cruz@gmail.com>
2020-06-06 16:41:21 -0500

    fix js functionality on loading grid on website reload

--- js/plugins.js
+++ js/plugins.js
@@ -214,7 +214,7 @@ $(document).ready(function () {
 
 /*========== PORTFOLIO FILTER (ISOTOPE) INITIALIZATION ==========*/
 var $grid = $('.grid').isotope({
- filter: '.web-design',
+ filter: '*',
  itemSelector: '.element-item',
  layoutMode: 'fitRows'
 });
